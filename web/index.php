642f6dc22b3f6ecd91a9eb82b15eb64e1889bbbc
Sasha <sasha@streeteasy.com>
2014-11-07 16:23:25 -0500

    test2

--- web/index.php
+++ web/index.php
@@ -6,6 +6,38 @@ use ColorThief\ColorThief;
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$app->after(function (Request $request, Response $response) {
+  $appl = $request->attributes->get('app');
+
+  if ($request->get('jsonp_callback') !== null 
+      && $request->getMethod() === 'GET') 
+  {
+     /** 
+      * Configured via ConfigServiceProvider (https://github.com/igorw/ConfigServiceProvider)
+      * Example:
+      *      
+      * $config['jsonp_able_route_patterns'] = array(
+      *     '/{query}', 
+      *     '/teaser/{amount}'
+      * );
+      */
+      $jsonpAbleRoutePatterns = $appl['jsonp_able_route_patterns'];
+      $actualRouteKey = str_replace('GET_', '', $request->attributes->get('_route'));
+      $routes = $appl['routes']->all();
+
+      foreach ($routes as $routeKey => $route) {
+          $routeKey = str_replace('GET_', '', $routeKey);
+          if ($routeKey === $actualRouteKey 
+              && !in_array($route->getPattern(), $jsonpAbleRoutePatterns)) 
+          {
+              return;
+          }
+      }
+      $response->headers->set('Content-Type', 'application/javascript');
+      $response->setContent($request->get('jsonp_callback') . '(' . $response->getContent() . ');');
+  }
+});
+
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
