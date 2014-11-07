5e4729ac61e68b12e20f986d4e19ff873682794a
Sasha <sasha@streeteasy.com>
2014-11-07 15:52:20 -0500

    test2

--- web/index.php
+++ web/index.php
@@ -1,23 +1,8 @@
 <?php
 
 require('../vendor/autoload.php');
+use ColorThief\ColorThief;
 
-$app = new Silex\Application();
-$app['debug'] = true;
-
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  $palette = ColorThief::getPalette('http://lokeshdhakar.com/projects/color-thief/img/photo1.jpg');
-  return 'Hello '.$palette;
-});
-
-$app->run();
+$palette = ColorThief::getPalette('http://lokeshdhakar.com/projects/color-thief/img/photo1.jpg');
 
 ?>
